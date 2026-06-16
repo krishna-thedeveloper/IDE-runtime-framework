ab0c9eac0e4b2236ff32ce4f89a662c869b8124d
Muthu krishnan <Muthu_krishnan@zohomail.in>
2026-06-16 23:10:00 +0530

    feat: add telescope fuzzy finder

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,63 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            local telescope = require("telescope")
+
+            telescope.setup({
+                defaults = {
+                    prompt_prefix = "  ",
+                    selection_caret = "> ",
+
+                    layout_config = {
+                        horizontal = {
+                            preview_width = 0.55,
+                        },
+                    },
+
+                    sorting_strategy = "ascending",
+                    layout_strategy = "horizontal",
+
+                    file_ignore_patterns = {
+                        "node_modules",
+                        ".git",
+                        "dist",
+                        "build",
+                        "target",
+                        ".next",
+                    },
+                },
+
+                pickers = {
+                    find_files = {
+                        hidden = true,
+                    },
+                    live_grep = {
+                        additional_args = function()
+                            return { "--hidden" }
+                        end,
+                    },
+                },
+
+                extensions = {
+                    fzf = {
+                        fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
+                    },
+                },
+            })
+
+            pcall(telescope.load_extension, "fzf")
+        end,
+    },
+
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+    },
+}
