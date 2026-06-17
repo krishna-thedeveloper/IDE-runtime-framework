f3aa977cd1cc4e16400e7994639a7f69b25be118
Muthu krishnan <Muthu_krishnan@zohomail.in>
2026-06-17 19:20:00 +0530

    feat: add editor plugins (comment, surround, pairs, oil)

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "numToStr/Comment.nvim",
+        opts = {},
+    },
+
+    {
+        "kylechui/nvim-surround",
+        opts = {},
+    },
+
+    {
+        "echasnovski/mini.pairs",
+        opts = {},
+    },
+
+    {
+        "stevearc/oil.nvim",
+        opts = {},
+    },
+}
