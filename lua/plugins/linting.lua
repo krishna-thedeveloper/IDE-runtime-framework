176467d7511ee85336400becb0d6a7ed455e298b
Muthu krishnan <Muthu_krishnan@zohomail.in>
2026-06-18 23:50:00 +0530

    fix: remove eslint_d linter to avoid false positives

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -6,10 +6,6 @@ return {
             events = { "BufWritePost", "BufReadPost", "InsertLeave" },
             linters_by_ft = {
                 lua = { "selene" },
-                javascript = { "eslint_d" },
-                typescript = { "eslint_d" },
-                javascriptreact = { "eslint_d" },
-                typescriptreact = { "eslint_d" },
             },
         },
         config = function(_, opts)
