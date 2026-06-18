314f6f6d4886718974110f7c90db010c938cb3f1
Muthu krishnan <Muthu_krishnan@zohomail.in>
2026-06-18 23:00:00 +0530

    feat: add persistence session management and debug support

---
+++ lua/plugins/debug.lua
@@ -0,0 +1,45 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "nvim-neotest/nvim-nio",
+        },
+        keys = {
+            { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+            { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
+                desc = "Conditional Breakpoint" },
+            { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+            { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+            { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
+            { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+            { "<leader>dO", function() require("dap").step_out() end, desc = "Step Out" },
+            { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+            { "<leader>du", function() require("dapui").toggle() end, desc = "Toggle DAP UI" },
+            { "<leader>dh", function() require("dap.ui.widgets").hover() end, desc = "DAP Hover" },
+        },
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+
+            dapui.setup()
+
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+
+            local wk = require("which-key")
+            wk.add({
+                { "<leader>d", group = "Debug" },
+            })
+        end,
+    },
+}
---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre",
+        opts = {
+            dir = vim.fn.stdpath("state") .. "/sessions/",
+            need = 1,
+            branch = true,
+        },
+        config = function(_, opts)
+            require("persistence").setup(opts)
+
+            local wk = require("which-key")
+            wk.add({
+                { "<leader>S", group = "Session" },
+            })
+
+            vim.keymap.set("n", "<leader>Ss", function()
+                require("persistence").load()
+            end, { desc = "Restore Session" })
+
+            vim.keymap.set("n", "<leader>Sl", function()
+                require("persistence").load({ last = true })
+            end, { desc = "Restore Last Session" })
+
+            vim.keymap.set("n", "<leader>Sd", function()
+                require("persistence").stop()
+            end, { desc = "Don't Save Session" })
+        end,
+    },
+}
