32e1c038b51bb2555d4866968e11f0c875ee7b0a
Muthu krishnan <Muthu_krishnan@zohomail.in>
2026-06-22 02:23:05 +0000

    refactor(keymaps): replace telescope keymaps with picker manager calls
    Use manager.picker instead of direct telescope.builtin calls.
    Add require for completion manager.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,20 +2,17 @@ require("themes")
 require("managers.density")
 require("managers.focus")
 require("managers.notifications")
+require("managers.completion")
+
+local picker = require("managers.picker")
 
 vim.keymap.set("n", "<leader>w", "<cmd>update<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Close window" })
 
-local function telescope(action)
-    return function()
-        require("telescope.builtin")[action]()
-    end
-end
-
-vim.keymap.set("n", "<leader>ff", telescope("find_files"), { desc = "Find files" })
-vim.keymap.set("n", "<leader>fg", telescope("live_grep"), { desc = "Live grep" })
-vim.keymap.set("n", "<leader>fb", telescope("buffers"), { desc = "Find buffers" })
-vim.keymap.set("n", "<leader>fo", telescope("oldfiles"), { desc = "Find old files" })
-vim.keymap.set("n", "<leader>fh", telescope("help_tags"), { desc = "Help tags" })
-vim.keymap.set("n", "<leader>gf", telescope("git_files"), { desc = "Git files" })
-vim.keymap.set("n", "<leader>gc", telescope("git_commits"), { desc = "Git commits" })
+vim.keymap.set("n", "<leader>ff", picker.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", picker.live_grep, { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", picker.buffers, { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>fo", picker.oldfiles, { desc = "Find old files" })
+vim.keymap.set("n", "<leader>fh", picker.help_tags, { desc = "Help tags" })
+vim.keymap.set("n", "<leader>gf", picker.git_files, { desc = "Git files" })
+vim.keymap.set("n", "<leader>gc", picker.git_commits, { desc = "Git commits" })
